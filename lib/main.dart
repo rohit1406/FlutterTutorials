b35c0884933779636a9d957005fb3cf79b7f197f
Rohit Muneshwar-CIC <rohit.muneshwar-cic@ibm.com>
2021-07-18 15:15:13 +0200

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tutorial/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,11 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-      child: Center(
-          child: Container(
-        child: Text("Welcome to Flutter!"),
-      )),
-    ));
+      home: HomePage(),
+    );
   }
 }
