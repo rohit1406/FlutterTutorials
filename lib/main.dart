a6e7fcb3a241bb04911d0dfc64a80c8c1c84d351
Rohit Muneshwar-CIC <rohit.muneshwar-cic@ibm.com>
2021-07-18 16:46:43 +0200

    Day 3 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tutorial/home_page.dart';
+import 'package:tutorial/pages/home_page.dart';
+import 'package:tutorial/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.amber),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
